d9947dc1eaf141e3381710a9e462bcb75d9d87be
Juanse1595 <jspbescos15@gmail.com>
2025-11-02 20:40:53 -0500

    + Adjusting POST handler to create task correctly

--- handlers/create-task.handler.js
+++ handlers/create-task.handler.js
@@ -9,22 +9,27 @@ const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
 const createTask = async (req, res) => {
-  const { taskId, name } = req.body;
+  const { taskId, title } = req.body;
   if (typeof taskId !== "string") {
     res.status(400).json({ error: '"taskId" must be a string' });
-  } else if (typeof name !== "string") {
-    res.status(400).json({ error: '"name" must be a string' });
+  } else if (typeof title !== "string") {
+    res.status(400).json({ error: '"title" must be a string' });
   }
 
   const params = {
     TableName: TASKS_TABLE,
-    Item: { taskId, name },
+    Item: {
+      taskId,
+      title,
+      completed: false,
+      createdAt: new Date().toISOString(),
+    },
   };
 
   try {
     const command = new PutCommand(params);
     await docClient.send(command);
-    res.json({ taskId, name });
+    res.json({ taskId, title });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Could not create task" });
