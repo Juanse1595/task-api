317d9de115bae006c476591f5b8c2cecfa39ba32
Juanse1595 <jspbescos15@gmail.com>
2025-11-02 21:43:44 -0500

    + Added get all endpoint

--- handler.js
+++ handler.js
@@ -3,17 +3,21 @@ const { getOneTask } = require("./handlers/get-one-task.handler.js");
 const express = require("express");
 const serverless = require("serverless-http");
 const createTask = require("./handlers/create-task.handler.js");
+const { getAllTasks } = require("./handlers/get-all-task.handler.js");
 
 const app = express();
 
 
 app.use(express.json());
 
-app.get("/", getHelloWorld)
 
+app.get("/tasks", getAllTasks)
 app.get("/tasks/:taskId", getOneTask);
 
 app.post("/tasks", createTask);
+
+// Route to check api health
+app.get("/health", getHelloWorld)
 
 app.use((req, res, next) => {
   return res.status(404).json({
